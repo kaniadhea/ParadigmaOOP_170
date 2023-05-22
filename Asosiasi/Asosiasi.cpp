02ad3db9d56bc58849c069c87b2d0f306a546b6c
kaniadhea <kaniadhea7@gmail.com>
2023-05-22 19:13:38 +0700

    int main

--- Asosiasi/Asosiasi.cpp
+++ Asosiasi/Asosiasi.cpp
@@ -1,20 +1,60 @@
-// Asosiasi.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
-
 #include <iostream>
+#include <vector>
+using namespace std;
 
-int main()
-{
-    std::cout << "Hello World!\n";
+class dokter;
+class pasien {
+public:
+	string nama;
+	vector<dokter*> daftar_dokter;
+	pasien(string pNama) :nama(pNama) {
+		cout << "Pasien \"" << nama << "\"ada\n";
+	}
+	~pasien() {
+		cout << "Pasien \"" << nama <<"\" tidak ada\n";
+	}
+	void tambahDoker(dokter*);
+	void cetakDokter();
+};
+
+class dokter {
+public:
+	string nama;
+	vector<pasien*> daftar_pasien;
+
+	dokter(string pNama) :nama(pNama) {
+		cout << "Dokter\""<<nama<<"\"tidak ada\n";
+	}
+	~dokter() {
+		cout << "Dokter \"" << nama << "\"tidak ada\n";
+	}
+
+	void tambahPasien(pasien*);
+	void cetakPasien();
+};
+
+void pasien::tambahDoker(dokter* pDokter) {
+	daftar_dokter.push_back(pDokter);
+}
+void pasien::cetakDokter() {
+	cout << "Daftar Dokter yang menangani pasien\"" << this->nama << "\":\n";
+	for (auto& a : daftar_dokter) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+void dokter::tambahPasien(pasien* pPasien) {
+	daftar_pasien.push_back(pPasien);
+	pPasien->tambahDoker(this);
+}
+void dokter::cetakPasien() {
+	cout << "Daftar Pasien dari dokter \"" << this->nama << "\":\n";
+	for (auto& a : daftar_pasien) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
 }
 
-// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
-// Debug program: F5 or Debug > Start Debugging menu
-
-// Tips for Getting Started: 
-//   1. Use the Solution Explorer window to add/manage files
-//   2. Use the Team Explorer window to connect to source control
-//   3. Use the Output window to see build output and other messages
-//   4. Use the Error List window to view errors
-//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
-//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
+int main() {
+	dokter* varDokter1 = new dokter("dr.Budi");
+}
