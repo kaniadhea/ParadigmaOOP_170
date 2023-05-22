05cf794b35d47a9d941e78549fb000eef2d87950
kaniadhea <kaniadhea7@gmail.com>
2023-05-22 17:15:20 +0700

    Add project files.

---
+++ memberIinitilist/memberIinitilist.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	const int id;
+	string nama;
+	float nilai;
+
+public:
+	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) { //Member Initialition List
+		//Definisi
+
+	}
+};
