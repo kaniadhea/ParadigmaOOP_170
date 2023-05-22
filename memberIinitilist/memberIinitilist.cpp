fec9c74961a8e2ccfef91ccb3d6da4dc0b418b9b
kaniadhea <kaniadhea7@gmail.com>
2023-05-22 17:18:26 +0700

    membuat data

--- memberIinitilist/memberIinitilist.cpp
+++ memberIinitilist/memberIinitilist.cpp
@@ -11,7 +11,19 @@ private:
 public:
 	mahasiswa(int pId, string pNama, float pNilai) :id(pId), nama(pNama), nilai(pNilai) { //Member Initialition List
 		//Definisi
+	}
 
-
+	~mahasiswa() {
+		cout << "Id =" << id << endl;
+		cout << "Nama =" << nama << endl;
+		cout << "Nilai =" << nilai << endl;
 	}
 };
+
+int main() {
+	mahasiswa mhs(12, "Asroni", 90.5);
+
+	return 0;
+}
+
+
