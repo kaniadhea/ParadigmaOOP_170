1549dd2621d166d6e47295028caed2d55810e8e5
kaniadhea <kaniadhea7@gmail.com>
2023-05-22 19:25:35 +0700

    memanggil data

--- mainJM/manusia.h
+++ mainJM/manusia.h
@@ -1,1 +1,20 @@
 #pragma once
+#ifndef MANUSIA_H
+#define MANUSIA_H
+#include <iostream>
+using namespace std;
+
+class manusia {
+public:
+	string name;
+	jantung varJantung;
+
+	manusia(string pName);
+	: name(pName) {
+		cout << name << " hidup\n";
+	}
+	~manusia() {
+		cout << name << "mati\n";
+	}
+};
+#endif
