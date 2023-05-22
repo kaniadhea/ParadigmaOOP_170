8f0f773837e036994f7777a9ca8c8e69de28b4cf
kaniadhea <kaniadhea7@gmail.com>
2023-05-22 17:45:36 +0700

    mainIa

--- mainIA/mainIA.cpp
+++ mainIA/mainIA.cpp
@@ -7,6 +7,23 @@ using namespace std;
 
 int main() {
 	ibu* varIbu = new ibu("dini");
-	ibu varibu2 = new ibu("novi");
+	ibu* varibu2 = new ibu("novi");
+	anak* varAnak1 = new anak("tono");
+	anak* varAnak2 = new anak("rini");
+	anak* varAnak3 = new anak("Dewi");
+
+	varIbu->tambahAnak(varAnak1);
+	varIbu->tambahAnak(varAnak2);
+	varibu2->tambahAnak(varAnak2);
+	varibu2->tambahAnak(varAnak1);
+
+	varIbu->cetakAnak();
+	varibu2->cetakAnak();
+
+	delete varIbu;
+	delete varibu2;
+	delete varAnak1;
+	delete varAnak2;
+	delete varAnak3;
 
 }
