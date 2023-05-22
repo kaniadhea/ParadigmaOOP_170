1aa4e9222ccdead8d280704d38e960a1115038d2
kaniadhea <kaniadhea7@gmail.com>
2023-05-22 17:34:26 +0700

    ibu

--- mainIA/ibu.h
+++ mainIA/ibu.h
@@ -1,1 +1,30 @@
 #pragma once
+#ifndef IBU_H
+#define IBU_H
+#include <iostream>
+#include "anak.h"
+#include <vector>
+using namespace std;
+
+class ibu {
+public: 
+	string nama;
+	vector<anak*>daftar_anak;
+
+	ibu(string pNama) :nama(pNama) {
+		cout << "Ibu\"" << nama << "\"tidak ada \n";
+	}
+	void tambahAnak(anak*);
+	void cetakAnak();
+};
+void ibu::tambahAnak(anak* pAnak) {
+	daftar_anak.push_back(pAnak);
+}
+void ibu::cetakAnak() {
+	cout << "daftar Anak dari Ibu \"" << this->nama << "\:n";
+	for (auto& a : daftar_anak) {
+		cout << a->nama << "\n";
+	}
+	cout << endl;
+}
+#endif
